41a93b83e2c11fda3afa37ca9254c3d77d9f482d
Minesh Tandel <minesh9174@gmail.com>
2022-05-22 15:50:52 -0400

    Updated class work with Final_Project submission

--- MIDTERM/RockPaperScissor/RockPaper.js
+++ MIDTERM/RockPaperScissor/RockPaper.js
@@ -1,3 +1,4 @@
+//Element access through DOUMENT OBJECT MODEL(DOM)  
 const choices = document.querySelectorAll('.choice1');
 const choices2 = document.querySelector('.choices2')
 const displayChoices = document.querySelector('.displayChoices')
@@ -9,34 +10,34 @@ const player1Name = document.querySelector('.player1_name')
 const player2Name = document.querySelector('.player2_name')
 const player1Score = document.querySelector('.player1_score')
 const player2Score = document.querySelector('.player2_score')
-let totalGamesPlayed = 0;
-let totalDraws = 0;
 
-// const scoreboard = {
-//     player: 0,
-//     computer: 0
-// }
+// Global variables initialization
+let totalGamesPlayed = 0; // Number of total games
+let totalDraws = 0; // Number of total draws
+const selectedValue = player2Name.value;
+console.log(selectedValue)
+
 
 // Computer As A Player
 function play(e) {
-    if(player2Name.options[player2Name.selectedIndex].value === 'notSelected') {
+    var selectedValue = player2Name.value;
+    console.log(selectedValue)
+    if(selectedValue === 'notSelected') {
         alert('Please choose player2 type')
     }
-    else if(player2Name.options[player2Name.selectedIndex].value === 'Computer') {
+    else if(player2Name.value === 'Computer') {
         restart.style.display = 'inline-block';
         modal.style.display = 'block';
         const player1Choice = e.target.id;
         const player1Image = e.target.src
-        console.log(player1Image)
-        const player2Choice = selectionPlayer2();
+        const player2Choice = selectionPlayer2(selectedValue);
         const player2Image = document.getElementById(player2Choice).src
-        console.log(player2Image)
         const winner = getWinner(player1Choice, player2Choice);
         showWinner(winner, player1Image, player2Image);
     }
-    else if(player2Name.options[player2Name.selectedIndex].value === 'Player2') {
+    // else if(player2Name.options[player2Name.selectedIndex].value === 'Player2') {
         
-    }
+    // }
 }
 
 // get computer's choice
@@ -80,12 +81,14 @@ function getWinner(p, c){
 
 function showWinner(winner, player1Image, player2Image){
     
+    // display the choice of player on UI
     displayChoices.innerHTML = `
         <img class='player1Image' src='${player1Image}'></img>
         <span class='saparateLine'>|</span>
         <img class='player2Image' src='${player2Image}'></img>
         `;
 
+    // Displays total number of games played / draws a
     if (winner === 'player'){
         //Inc player score
         totalGamesPlayed++;
@@ -111,7 +114,8 @@ function showWinner(winner, player1Image, player2Image){
         <p>Draws: ${totalDraws}</p>
         `;
     }
-    //show score
+
+    //show  changed score
     score.innerHTML = `
     <p class="player1">
         <span class="player1_name">${player1Name.innerHTML}</span>
@@ -125,37 +129,35 @@ function showWinner(winner, player1Image, player2Image){
     </p>`;
 }
 
-//Restart Game
+//on-Clicking Play-Again button restartGame function will be triggered
 function restartGame(){
-    player1Score.innerHTML = 0;
-    player2Score.innerHTML = 0;
-    totalGamesPlayed = 0;
-    totalDraws = 0;
-    player1Name.innerHTML  = 'Player1';
+    player1Score.innerHTML = 0; // changes player1 score to - 0
+    player2Score.innerHTML = 0; // changes player2 score to - 0
+    totalGamesPlayed = 0; // changes number of games played to - 0
+    totalDraws = 0; // changes number of draws to - 0
+
     //changing innerHTML of score and player name upon restart as it was in the beginning of the game
     score.innerHTML = 
     `<p class="player1">
         <span class="player1_name">Player1</span>
-        : 
+        :
         <span class="player1_score">0</span>
     </p>
     <p class="player2">
         <select class="player2_name">
             <option value="notSelected">Choose Player2</option>
             <option value="Computer">Computer</option>
-            // <option value="Player2"">Player2</option>
+            <option value="Player2"">Player2</option>
         </select>
         : 
         <span class="player2_score">0</span>
     </p>`;
-    restart.style.display = 'none';
-    modal.style.display = 'none';
-    displayChoices.style.display = 'none';
+    restart.style.display = 'none'; // It will deactivate Play Again button
+    modal.style.display = 'none'; 
+    displayChoices.style.display = 'none'; // it will deactivate choice displaying section
 }
 
-function selectionPlayer2() {
-    var selectedValue = player2Name.options[player2Name.selectedIndex].value;
-
+function selectionPlayer2(selectedValue) {
     if(selectedValue === "Computer") {
        return getComputerChoice();
     }
@@ -168,15 +170,20 @@ function clearModal(e){
     }
 }
 
+//Player Name-change functionality
 function changeName() {
+    // prompts user a window to enter his/her name and assign it to the player1 name section
     const userName = prompt("enter your name: ");
     if (userName !== '') {
         player1Name.innerHTML = userName;
     }
 }
 
-function displayPlayerChoice(e){
-    if(player2Name.options[player2Name.selectedIndex].value === 'Computer'){
+
+//To display both player choice on UI
+function displayPlayerChoice(){
+    selectedValue = player2Name.value;
+    if(selectedValue === 'Computer'){
         displayChoices.innerHTML = `
         <span class='player1Image'></span>
         <span class='saparateLine'>|</span>
@@ -185,9 +192,9 @@ function displayPlayerChoice(e){
     }
 }
 
-//Event listeners
-choices.forEach(choice => choice.addEventListener('click', play));
-window.addEventListener('click', clearModal);
-restart.addEventListener('click', restartGame);
-player1Name.addEventListener('click', changeName);
+//Event Addition to the elements on perticular actions
+
+choices.forEach(choice => choice.addEventListener('click', play)); // On-Clicking choices it will trigger play() function
+restart.addEventListener('click', restartGame); // On-Clicking restart button restarGame() function will be executed
+player1Name.addEventListener('click', changeName); // click on player1 will prompt a user to change his/her name
 player2Name.addEventListener('change', displayPlayerChoice)
--- MIDTERM/WarmUpExercise/DOM CRASH COURSE/dom.js
+++
@@ -1,5 +0,0 @@
-const heading = document.querySelector('#heading');
-console.log(heading.innerText) //innerText takes into consideration of applied style to the element
-console.log(heading.textContent) //textContent ignores applied style
-
-console.log(heading.innerHTML) // returns all elements along with all nodes present
